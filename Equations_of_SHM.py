110471c0b9d0335bf73343e36162438d3cfcfb0f
https://astanakitap.kz/ <44795644+astana-kitap@users.noreply.github.com>
2018-11-06 17:30:26 +0600

    Update Equations_of_SHM.py

--- Equations_of_SHM.py
+++ Equations_of_SHM.py
@@ -2,11 +2,15 @@ GlowScript 2.7 VPython
 
 
 #Change omega value
-omega=0.5
-
-
-
+omega=1
 amplitude=5
+
+
+
+
+
+
+##############################################################
 x_axis=arrow(pos=vector(0,1.5,0), axis=vector(amplitude+2,0,0), shaftwidth=0.05)
 y_axis=arrow(pos=vector(0,1.5,0), axis=vector(0,amplitude+2,0), shaftwidth=0.05)
 
@@ -14,10 +18,15 @@ y_axis=arrow(pos=vector(0,1.5,0), axis=vector(0,amplitude+2,0), shaftwidth=0.05)
 ball=sphere(pos=vector(amplitude,1.5,0), radius=0.3, 
             color=color.green, make_trail=True)
 
-spring=helix(pos=vector(-(amplitude+3),-(amplitude+2),0), axis=vector(1,0,0), 
-             radius=0.5, length=2*amplitude+3, coils=15)
+
 
 square =box(pos=vector(amplitude,-(amplitude+2),0 ), size=vector(1,1,1))
+
+distance=square.pos.x+amplitude+3
+
+spring=helix(pos=vector(-(amplitude+3),-(amplitude+2),0), 
+             axis=vector(distance,0,0),
+             radius=0.5, coils=15)
 
 wall=box(pos=vector(-(amplitude+3),-(amplitude+2),0), size=vector(0.1, 3,3) )
 
@@ -27,6 +36,8 @@ dt=0.005
 
 
 while True:
-    rate(200)
+    rate(300)
     ball.rotate(angle=omega*dt, axis=vector(0,0,1), origin=vector(0,1.5,0))
-    
+    t=t+dt
+    square.pos.x=amplitude*cos(omega*t)
+    spring.axis=square.pos-spring.pos
